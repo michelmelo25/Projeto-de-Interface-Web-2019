b8cb65874894445a3682c84bed04edef778fe0e4
Michel Melo <michelmelosj25@hotmail.com>
2019-04-27 17:19:52 -0300

    exercicio post atualizada

--- Exercicios/Codigos/Post/src/app/linha-do-tempo/linha-do-tempo.component.ts
+++ Exercicios/Codigos/Post/src/app/linha-do-tempo/linha-do-tempo.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-linha-do-tempo',
   templateUrl: './linha-do-tempo.component.html',
   styleUrls: ['./linha-do-tempo.component.css']
 })
-export class LinhaDoTempoComponent implements OnInit {
+export class LinhaDoTempoComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  posts = []
+  contador=0;
+  enviar(nome:string, texto:string){
+    this.posts.push({id: this.contador, nome:nome, texto:texto, likes:0});
+    this.contador++
+    console.log("Teste: ");
+    this.posts.forEach(function(post){
+      console.log(post)
+    })
   }
 
+  findPost(id:number):any{
+    for(let post of this.posts){
+      if(post.id==id) 
+        post.likes++;
+      }
+    }
 }
--- Exercicios/Codigos/Post/src/app/post-da-linha/post-da-linha.component.ts
+++ Exercicios/Codigos/Post/src/app/post-da-linha/post-da-linha.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-post-da-linha',
   templateUrl: './post-da-linha.component.html',
   styleUrls: ['./post-da-linha.component.css']
 })
-export class PostDaLinhaComponent implements OnInit {
+export class PostDaLinhaComponent {
 
-  constructor() { }
+  @Output() id = new EventEmitter;
 
-  ngOnInit() {
+  @Input() post:{
+    id: Number,
+    nome: String,
+    texto: String,
+    likes: Number
+  };  
+
+  like(){
+    this.id.emit(this.post.id);
   }
-
 }
